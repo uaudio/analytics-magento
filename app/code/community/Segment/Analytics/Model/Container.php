9258318fec9aaa5a3bbd5f42c657b452b84232b7
Jay Thomas <jay@uaudio.com>
2015-05-26 12:02:35 -0700

    simplify the cache container

--- app/code/community/Segment/Analytics/Model/Container.php
+++ app/code/community/Segment/Analytics/Model/Container.php
@@ -5,44 +5,7 @@
  * @package    Segment_Analytics
  */
 class Segment_Analytics_Model_Container  extends Enterprise_PageCache_Model_Container_Abstract {
-
-	/**
-	 * @ignore
-	 */
-	protected function _getIdentifier() {
-		$block = $this->_placeholder->getAttribute('block');
-		return $block.$_COOKIE['PHPSESSID'];
-	}
-
-	/**
-	 * @ignore
-	 */
-	protected function _getCacheId() {
-		return 'ANALYTICS_CONTAINER' . md5($this->_placeholder->getAttribute('cache_id') . $this->_getIdentifier());
-	}
-
-	/**
-	 * @ignore
-	 */
-	protected function _renderBlock() {
-		$block = $this->_placeholder->getAttribute('block');
-		$template = $this->_placeholder->getAttribute('template');
-
-		$block = new $block;
-		$block->setTemplate($template);
-		$block->setLayout(Mage::app()->getLayout());
-
-		return $block->toHtml();
-	}
-
-	/**
-	 * @ignore
-	 */
-	public function saveCache($blockContent) {
-		return $this;
-	}
-	
 	public function applyWithoutApp(&$content) {
-        	return false;
-    	}
+	    return false;
+    }
 }
