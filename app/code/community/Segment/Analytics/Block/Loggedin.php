74b5dddb437e7d7165754e4de7a00faef3882a91
Jay Thomas <jay@uaudio.com>
2015-05-27 09:22:08 -0700

    rm a debug js alert

--- app/code/community/Segment/Analytics/Block/Loggedin.php
+++ app/code/community/Segment/Analytics/Block/Loggedin.php
@@ -2,6 +2,6 @@
 class Segment_Analytics_Block_Loggedin extends Mage_Core_Block_Template {
 	public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
-		return '<script>alert('.time().'); document.observe("dom:loaded", function() { window.analytics.track(\'Logged In\',{});});</script>';
+		return '<script>document.observe("dom:loaded", function() { window.analytics.track(\'Logged In\',{});});</script>';
 	}
 }
