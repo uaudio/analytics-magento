3108de42f173a76b6f3028c9592e0c896db8b841
Jay Thomas <jay@uaudio.com>
2015-05-27 14:05:30 -0700

    add a few more events to observer

--- app/code/community/Segment/Analytics/Model/Observer.php
+++ app/code/community/Segment/Analytics/Model/Observer.php
@@ -15,6 +15,39 @@ class Segment_Analytics_Model_Observer {
         $this->addDeferredAction('loggedin');
     }
     
+    public function loggedOut($observer) {
+        $this->addDeferredAction('loggedout');
+    }
+    
+    public function addToCart($observer) {
+        $product = $observer->getProduct();
+        $this->addDeferredAction('addtocart',
+            array('sku'=>$product->getSku())
+        );
+    }
+
+    public function removeFromCart($observer) {
+        $product = $observer->getQuoteItem()->getProduct();
+        $this->addDeferredAction('removefromcart',
+            array('sku'=>$product->getSku())
+        );
+    }
+
+    public function loadedSearch($observer) {
+        $o = $observer->getDataObject();
+        if(!$o->getQueryText()){return;}
+        $this->addAction('loadedsearch',
+            array('query'=>$o->getQueryText())
+        );
+    }
+    
+    public function customerRegistered($observer) {
+        $customer = $observer->getCustomer();
+        $this->addDeferredAction('registered',
+            array('customer_id'=>$customer->getEntityId())
+        );
+    }
+
     public function _getSession() {
         return Mage::getSingleton('segment_analytics/session');
     }
@@ -26,6 +59,9 @@ class Segment_Analytics_Model_Observer {
         $session = $this->_getSession();
         $deferred = $session->getDeferredActions();
         $deferred = $deferred ? $deferred : array();
+        foreach ($deferred as $i => $_action) {
+            if ($_action->action == $action) unset($deferred[$i]);
+        }
         $deferred[] = $o_action;
         $session->setDeferredActions($deferred);
         return $this;
@@ -38,6 +74,24 @@ class Segment_Analytics_Model_Observer {
     
     public function getDeferredActions() {
         $actions = $this->_getSession()->getDeferredActions();
+        $actions = $actions ? $actions : array();
+        Mage::log($actions);
+        return $actions;
+    }
+
+    public function addAction($action, $action_data=array()) {
+        $o_action = new stdClass;
+        $o_action->action = $action;
+        $o_action->data = $action_data;
+        $actions = Mage::registry('actions');
+        $actions[] = $o_action;
+        Mage::unregister('actions');
+        Mage::register('actions',$actions);
+        return $this;
+    }
+    
+    public function getActions() {
+        $actions = Mage::registry('actions');
         $actions = $actions ? $actions : array();
         return $actions;
     }
