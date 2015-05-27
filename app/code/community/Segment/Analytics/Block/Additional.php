9176fd3bc3f63fd0ea6c8e719485bc8032e39d3f
Jay Thomas <jay@uaudio.com>
2015-05-27 14:04:04 -0700

    get actions, deferred actions, and register data for blocks

--- app/code/community/Segment/Analytics/Block/Additional.php
+++ app/code/community/Segment/Analytics/Block/Additional.php
@@ -1,16 +1,23 @@
 <?php
 class Segment_Analytics_Block_Additional extends Mage_Core_Block_Template {
-    public $actions;
+    public $actions = array();
+    public $deferredActions = array();
     
     public function __construct() {
-        $this->actions = Mage::getModel('segment_analytics/observer')->getDeferredActions();
+        $this->actions = Mage::getModel('segment_analytics/observer')->getActions();
+        $this->deferredActions = Mage::getModel('segment_analytics/observer')->getDeferredActions();
         Mage::getModel('segment_analytics/observer')->clearDeferredActions();
     }
 
 	public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
 	    $script = '';
+	    foreach ($this->deferredActions as $action) {
+	        Mage::register('segment_data',$action->data);
+	        $script .= Mage::app()->getLayout()->createBlock('analytics/' . $action->action)->toHtml();
+	    }
 	    foreach ($this->actions as $action) {
+	        Mage::register('segment_data',$action->data);
 	        $script .= Mage::app()->getLayout()->createBlock('analytics/' . $action->action)->toHtml();
 	    }
 	    return $script;
