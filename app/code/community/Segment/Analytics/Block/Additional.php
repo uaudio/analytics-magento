ad1e246bb5816f63c2e7f41916a65a4a3323cdaf
Jay Thomas <jay@uaudio.com>
2015-05-27 14:56:17 -0700

    refactor

--- app/code/community/Segment/Analytics/Block/Additional.php
+++ app/code/community/Segment/Analytics/Block/Additional.php
@@ -1,21 +1,15 @@
 <?php
 class Segment_Analytics_Block_Additional extends Mage_Core_Block_Template {
     public $actions = array();
-    public $deferredActions = array();
     
     public function __construct() {
-        $this->actions = Mage::getModel('segment_analytics/observer')->getActions();
-        $this->deferredActions = Mage::getModel('segment_analytics/observer')->getDeferredActions();
+        $this->actions = array_merge(Mage::getModel('segment_analytics/observer')->getActions(),Mage::getModel('segment_analytics/observer')->getDeferredActions());
         Mage::getModel('segment_analytics/observer')->clearDeferredActions();
     }
 
 	public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
 	    $script = '';
-	    foreach ($this->deferredActions as $action) {
-	        Mage::register('segment_data',$action->data);
-	        $script .= Mage::app()->getLayout()->createBlock('analytics/' . $action->action)->toHtml();
-	    }
 	    foreach ($this->actions as $action) {
 	        Mage::register('segment_data',$action->data);
 	        $script .= Mage::app()->getLayout()->createBlock('analytics/' . $action->action)->toHtml();
