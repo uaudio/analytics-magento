cb986defb0eb2961bbb98478bd0ddf6d26055540
Jay Thomas <jay@uaudio.com>
2015-06-03 13:12:57 -0700

    don't throw exception if no session

--- app/code/community/Segment/Analytics/Model/Session.php
+++ app/code/community/Segment/Analytics/Model/Session.php
@@ -1,7 +1,6 @@
 <?php
 class Segment_Analytics_Model_Session extends Mage_Core_Model_Session_Abstract {
     public function __construct() {
-        if(!session_id()) Mage::throwException("No session id. Blocking rather than instantiate object early.");
-        $this->init('segment_analytics');
+        if(session_id()) $this->init('segment_analytics');
     }
 }
