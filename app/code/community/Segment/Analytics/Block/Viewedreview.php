a78645d06c131edabf0943d253d934a2e77d3a6a
Jay Thomas <jay@uaudio.com>
2015-06-04 20:04:46 -0700

    Fix viewed product reviews

--- app/code/community/Segment/Analytics/Block/Viewedreview.php
+++ app/code/community/Segment/Analytics/Block/Viewedreview.php
@@ -1,9 +1,9 @@
 <?php
-class Segment_Analytics_Block_Viewedproduct extends Mage_Core_Block_Template {
+class Segment_Analytics_Block_Viewedreview extends Mage_Core_Block_Template {
 	public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
         $data = Mage::registry('segment_data');
         Mage::unregister('segment_data');
-		return '<script>document.observe("dom:loaded", function() { window.analytics.track(\'Viewed Product\',{\'category\':\'Product\',\'sku\':\''.$data['sku'].'\'});});</script>';
+		return '<script>document.observe("dom:loaded", function() { window.analytics.track(\'Viewed Product Reviews\',{\'category\':\'Product\',\'sku\':\''.$data['sku'].'\'});});</script>';
 	}
 }
