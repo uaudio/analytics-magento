82af7a5ce45c41ddb226f92e4d4234c043d98c8a
Jay Thomas <jay@uaudio.com>
2015-06-09 10:23:14 -0700

    Get page track category from getRequestedRouteName

--- app/code/community/Segment/Analytics/Block/Page.php
+++ app/code/community/Segment/Analytics/Block/Page.php
@@ -3,6 +3,14 @@ class Segment_Analytics_Block_Page extends Mage_Core_Block_Template {
     public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
         $contextJson = Mage::helper('analytics')->getContextJson();
-		return '<script>document.observe("dom:loaded", function() { window.analytics.page("'.(is_object($this->getLayout()->getBlock('head'))?$this->getLayout()->getBlock('head')->getTitle():'').'",{},'.$contextJson.');});</script>';
+		return '<script>document.observe("dom:loaded", function() { window.analytics.page("'.$this->_getCategoryName().'","'.$this->_getTitle().'",{},'.$contextJson.');});</script>';
+	}
+	
+	protected function _getCategoryName() { 
+	    return ucfirst($this->getRequest()->getRequestedRouteName());
+	}
+
+	protected function _getTitle() {
+	    return is_object($this->getLayout()->getBlock('head'))?$this->getLayout()->getBlock('head')->getTitle():'';
 	}
 }
