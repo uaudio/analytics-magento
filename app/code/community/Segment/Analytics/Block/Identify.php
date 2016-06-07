dccb5438012575ca8a5d59c6b7270d64668c8cc6
Jay Thomas <jay@uaudio.com>
2016-06-07 10:25:00 -0700

    json_encode the $customerInfo instead of using a foreach that left a trailing comma in the json

--- app/code/community/Segment/Analytics/Block/Identify.php
+++ app/code/community/Segment/Analytics/Block/Identify.php
@@ -1,17 +1,13 @@
 <?php
-class Segment_Analytics_Block_Identify extends Mage_Core_Block_Template {
-	public function _toHtml() {
+    class Segment_Analytics_Block_Identify extends Mage_Core_Block_Template {
+    public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
-	    if (Mage::getSingleton('customer/session')->isLoggedIn()) {
-	        $contextJson = Mage::helper('analytics')->getContextJson();
-	        $customer = Mage::getSingleton('customer/session')->getCustomer();
-	        $customerInfo = Mage::helper('analytics')->getCustomerInformation($customer->getData());
-	        $code = '<script>document.observe("dom:loaded", function() { window.analytics.identify("'.$customer->getId().'", {';
-	        foreach ($customerInfo as $attribute=>$value) {
-	            $code .= $attribute . ': '. json_encode($value) . ',';
-	        }
-            $code .= '},'.$contextJson.');   });</script>';
+        if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $contextJson = Mage::helper('analytics')->getContextJson();
+            $customer = Mage::getSingleton('customer/session')->getCustomer();
+            $customerInfo = Mage::helper('analytics')->getCustomerInformation($customer->getData());
+            $code = '<script>document.observe("dom:loaded", function() { window.analytics.identify("'.$customer->getId().'", ' . json_encode($customerInfo) . ','.$contextJson.');   });</script>';
             return $code;
         }
-	}
+    }
 }
