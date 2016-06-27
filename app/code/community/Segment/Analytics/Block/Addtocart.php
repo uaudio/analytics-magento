6ab70dfc1593d9e30289785c0aaf46b42f0c6ab4
Jay <ranchoweb@gmail.com>
2016-06-27 14:44:27 -0700

    Include all data with Add to Cart call

--- app/code/community/Segment/Analytics/Block/Addtocart.php
+++ app/code/community/Segment/Analytics/Block/Addtocart.php
@@ -3,7 +3,6 @@ class Segment_Analytics_Block_Addtocart extends Segment_Analytics_Block_Abstract
 
     public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
-        $data = $this->getActionData();
-        return $this->track('Added Product', ['sku' => $data['sku']]);
+        return $this->track('Added Product', $this->getActionData());
     }
 }
