410dd0cbf09b87c60913508f6f78b576d53fd46f
Jay <ranchoweb@gmail.com>
2016-06-27 15:46:56 -0700

    need to send all data with Removed Product event

--- app/code/community/Segment/Analytics/Block/Removefromcart.php
+++ app/code/community/Segment/Analytics/Block/Removefromcart.php
@@ -4,7 +4,6 @@ class Segment_Analytics_Block_Removefromcart extends Segment_Analytics_Block_Abs
 
     public function _toHtml() {
         if (!Mage::helper('analytics')->isEnabled()) return false;
-        $data = $this->getActionData();
-        return $this->track('Removed Product', ['sku' => $data['sku']]);
+        return $this->track('Removed Product', $this->getActionData());
     }
 }
